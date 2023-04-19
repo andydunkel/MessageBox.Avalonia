c187eb13095867695704f322fb941f63b04ec7e8
Andy Dunkel <a.dunkel@pilz.de>
2023-04-19 11:06:32 +0200

    Added translations

--- src/MessageBox.Avalonia.Example/App.xaml.cs
+++ src/MessageBox.Avalonia.Example/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System.Globalization;
+using System.Threading;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
@@ -10,6 +12,14 @@ public class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        //test code to set different culture
+        CultureInfo myCultureInfo = new CultureInfo("fr");
+        CultureInfo.DefaultThreadCurrentCulture = myCultureInfo;
+        CultureInfo.DefaultThreadCurrentUICulture = myCultureInfo;
+
+        Thread.CurrentThread.CurrentCulture = myCultureInfo;
+        Thread.CurrentThread.CurrentUICulture = myCultureInfo;
     }
 
     public override void OnFrameworkInitializationCompleted()
